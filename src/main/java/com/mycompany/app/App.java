fd4d791c195eb63e8d9c08550254358bc3046635
harrislukman13 <58070313+harrislukman13@users.noreply.github.com>
2023-04-27 14:06:09 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,13 +6,13 @@ package com.mycompany.app;
 public class App {
 
     private static final String MESSAGE = "Hello World!";
-
+    private static int i = 0;
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
 
-        if(MESSAGE == "Hello World!"){
+        if(MESSAGE == "Hello World!" & i !=0 ){
             System.out.println(true);
         }
     }
